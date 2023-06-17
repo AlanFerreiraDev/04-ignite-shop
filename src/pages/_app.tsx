2161bd5fe5b41f98786fe6c26f2b07cba7aca6c4
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-06-16 23:14:28 -0300

    feat: create a global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages'
+import Image from 'next/image'
+
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={130} height={52} />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
